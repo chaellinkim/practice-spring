e3a84a7b39423bda0d07177f8bc87b84f5916be3
clin0927 <clin0927@naver.com>
2023-05-27 02:10:22 +0900

    jpa update

---
+++ src/main/java/practice/practicespring/service/SpringConfig.java
@@ -0,0 +1,29 @@
+package practice.practicespring.service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import practice.practicespring.repository.MemberRepository;
+
+@Configuration
+public class SpringConfig {
+//    private final MemberRepository memberRepository;
+
+
+//    public SpringConfig(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+//    @Bean
+//    public MemberService memberService(){
+//        return new MemberService(memberRepository);
+//    }
+
+//    @Bean
+//    public MemberService memberRepository(){
+//
+//    }
+//      @Bean
+//      public TimeTraceAop timeTraceAop(){
+//          return new TimeTraceAop();
+//      }
+}
